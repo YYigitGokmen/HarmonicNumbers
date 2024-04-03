42ae3664e8eec1004a0dac15e20c51c1627c50c8
Yılmaz Yiğit Gökmen <157407435+YYigitGokmen@users.noreply.github.com>
2024-04-03 22:29:48 +0300

    Add files via upload

---
+++ HarmonicNumber.java
@@ -0,0 +1,48 @@
+
+
+import java.util.Scanner;
+
+
+
+public class HarmonicNumber {
+public static void main(String[]args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.print("N sayısını giriniz : ");
+    int n= scan.nextInt();
+    double result = 0;
+
+        for (int i = 1; i<=n;i++){
+                result += (1.0/i);
+
+        }
+    System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
